139ab188fcabdd27612c493efb2a54d1e9a0cd8b
Jan-Marius Vatle <48485965+janmarius@users.noreply.github.com>
2025-10-19 00:12:34 +0200

    make cursor change depending on ViMode in PowerShell

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
-Set-PSReadLineOption -EditMode Vi
 oh-my-posh init pwsh --config "$HOME\Dev\dotfiles-windows\oh-my-posh\paradox-dark-plus.omp.json" | Invoke-Expression
 
 function dev {
@@ -8,3 +7,22 @@ function dev {
 function dot {
     Set-Location "$HOME\Dev\dotfiles-windows"
 }
+
+Set-PSReadLineOption -EditMode Vi
+Write-Host -NoNewline "`e[6 q"
+
+function OnViModeChange($mode) {
+    switch ($mode) {
+        'Command' {
+            # NORMAL mode: solid block cursor
+            Write-Host -NoNewline "`e[2 q"
+        }
+        'Insert' {
+            # INSERT mode: blinking bar cursor
+            Write-Host -NoNewline "`e[6 q"
+        }
+    }
+}
+
+# Tell PSReadLine to call our handler whenever the mode changes
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
